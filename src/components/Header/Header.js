b0696525965c92fe6c096c924ccc0c85ec0e2f6c
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-03-31 05:22:54 +0600

    name changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Pet Shop</h1>
+            <h1>Lucky Pet Shop</h1>
             <h2>Choose 4 Pets</h2>
         </div>
     );
